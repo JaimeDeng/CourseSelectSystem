5fc70aa17966624945090d165c422f5da9b82630
Jaime <raven0980642580@gmail.com>
2023-04-08 23:50:19 +0800

    First commt

---
+++ src/test/java/com/example/CourseSelectSystem/CourseSelectSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseSelectSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
